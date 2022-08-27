779b1422ff37af24e477b68afe0f593c23639ab1
Jordan Cook <jordan.cook@telrite.com>
2022-08-27 00:11:28 -0400

    fixed deploy

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:jcook3195/house-build.git main:gh-pages
+git push -f git@github.com:jcook3195/house-build.git master:gh-pages
 
 cd -
